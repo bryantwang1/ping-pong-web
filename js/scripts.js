620fb5fd440c22c5da6e7abe8243427f73e7d712
Bryant Wang <bryant.m.wang@gmail.com>
2016-10-28 08:38:18 -0700

    Add basic ui js to scripts.js.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+
+// UI below this line
+
+$(function() {
+  $("form#ping-ponging").submit(function(event){
+    event.preventDefault();
+    $(".result").append("<ul></ul>");
+
+    var userNumber = $("input#user-number").val();
+    pingPonger(userNumber);    
+  });
+});
